2c3537075236c8680ec4c1f449d0049f5bedfd06
Thorgalix <lucaspodevin@sfr.fr>
2026-05-22 10:23:53 +0200

    Ajout de la création d'un tableau HTML pour afficher la liste des étudiants

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -20,3 +20,23 @@ const student2: Student = {
 }
 
 const studentsList: Student[] = [student1, student2]
+
+/**Créer un tableau html*/
+
+const table = document.createElement("table");
+
+studentsList.forEach((student) => {
+  const row = document.createElement("tr");
+
+  const nameCell = document.createElement("td");
+  nameCell.textContent = student.firstName;
+
+  const locationCell = document.createElement("td");
+  locationCell.textContent = student.location;
+
+  row.appendChild(nameCell);
+  row.appendChild(locationCell);
+  table.appendChild(row);
+});
+
+document.body.appendChild(table);
